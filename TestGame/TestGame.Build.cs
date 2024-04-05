61fd0c134cc20ebb7327dc260d43bdb8ca9fbe35
문경현 <ghoflvhxj@gmail.com>
2024-04-06 04:42:11 +0900

    UI 작업을 위한 모듈추가

--- TestGame/TestGame.Build.cs
+++ TestGame/TestGame.Build.cs
@@ -8,11 +8,11 @@ public class TestGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "GameplayTags", "OnlineSubsystem", "Sockets", "Networking", "GameplayAbilities", "GameplayTasks" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "GameplayTags", "OnlineSubsystem", "Sockets", "Networking", "GameplayAbilities", "GameplayTasks", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" });
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
