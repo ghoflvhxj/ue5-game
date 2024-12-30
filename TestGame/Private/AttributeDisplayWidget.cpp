5a627ef2f842c1a7ea59840470b371b4ca661cb3
문경현 <ghoflvhxj@gmail.com>
2024-12-30 17:23:42 +0900

    어트리뷰트 위젯 수정

--- TestGame/Private/AttributeDisplayWidget.cpp
+++ TestGame/Private/AttributeDisplayWidget.cpp
@@ -1,39 +1,78 @@
 #include "AttributeDisplayWidget.h"
 #include "AbilitySystemComponent.h"
 
+bool UActorBindWidget::BindActor(AActor* InActor)
+{
+	if (InActor != BoundActor)
+	{
+		AActor* Old = BoundActor;
+		BoundActor = InActor;
+		OnBoundActorChanged(Old, BoundActor);
+
+		return true;
+	}
+
+	return false;
+}
+
+void UActorBindWidget::OnBoundActorChanged_Implementation(AActor* Old, AActor* New)
+{
+	if (OnActorChangedEvent.IsBound())
+	{
+		OnActorChangedEvent.Broadcast(Old, New);
+	}
+}
+
 void UAttributeDisplayWidget::OnCurrentAttributeValueChanged(const FOnAttributeChangeData& AttributeChangeData)
 {
 	UpdateCurrentAttributeValue(AttributeChangeData.OldValue, AttributeChangeData.NewValue);
 }
-
 
 void UAttributeDisplayWidget::OnMaxAttributeValueChanged(const FOnAttributeChangeData& AttributeChangeData)
 {
 	UpdateMaxAttributeValue(AttributeChangeData.OldValue, AttributeChangeData.NewValue);
 }
 
-void UAttributeDisplayWidget::BindAttributeOwner(AActor* InAttributeOwner)
-{
-	if (IsValid(InAttributeOwner) == false || AttributeOwner == InAttributeOwner)
-	{
-		return;
-	}
 
-	if (IsValid(AttributeOwner))
+void UAttributeDisplayWidget::OnBoundActorChanged_Implementation(AActor* Old, AActor* New)
+{
+	Super::OnBoundActorChanged_Implementation(Old, New);
+
+	if (IsValid(Old))
 	{
-		if (UAbilitySystemComponent* AbilitySystemComponent = AttributeOwner->GetComponentByClass<UAbilitySystemComponent>())
+		if (UAbilitySystemComponent* AbilitySystemComponent = Old->GetComponentByClass<UAbilitySystemComponent>())
 		{
 			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(CurrentAttribute).Remove(CurrentDelegate);
-			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(CurrentAttribute).Remove(MaxDelegate);
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(MaxAttribute).Remove(MaxDelegate);
+			CurrentDelegate.Reset();
+			MaxDelegate.Reset();
 		}
 	}
 
-	AttributeOwner = InAttributeOwner;
-	if (UAbilitySystemComponent* AbilitySystemComponent = AttributeOwner->GetComponentByClass<UAbilitySystemComponent>())
+	if (IsValid(New))
 	{
-		CurrentDelegate = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(CurrentAttribute).AddUObject(this, &UAttributeDisplayWidget::OnCurrentAttributeValueChanged);
-		MaxDelegate = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(MaxAttribute).AddUObject(this, &UAttributeDisplayWidget::OnMaxAttributeValueChanged);
-	}
+		if (UAbilitySystemComponent* AbilitySystemComponent = New->GetComponentByClass<UAbilitySystemComponent>())
+		{
+			CurrentDelegate = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(CurrentAttribute).AddUObject(this, &UAttributeDisplayWidget::OnCurrentAttributeValueChanged);
+			MaxDelegate = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(MaxAttribute).AddUObject(this, &UAttributeDisplayWidget::OnMaxAttributeValueChanged);
 
-	OnAttributeOwnerChanged(AttributeOwner);
+			if (CurrentAttribute.IsValid())
+			{
+				FOnAttributeChangeData AttributeChangeData;
+				AttributeChangeData.Attribute = CurrentAttribute;
+				AttributeChangeData.OldValue = 0.f;
+				AttributeChangeData.NewValue = AbilitySystemComponent->GetNumericAttribute(CurrentAttribute);
+				OnCurrentAttributeValueChanged(AttributeChangeData);
+			}
+
+			if (MaxAttribute.IsValid())
+			{
+				FOnAttributeChangeData AttributeChangeData;
+				AttributeChangeData.Attribute = MaxAttribute;
+				AttributeChangeData.OldValue = 0.f;
+				AttributeChangeData.NewValue = AbilitySystemComponent->GetNumericAttribute(MaxAttribute);
+				OnCurrentAttributeValueChanged(AttributeChangeData);
+			}
+		}
+	}
 }
--- TestGame/Public/AttributeDisplayWidget.h
+++ TestGame/Public/AttributeDisplayWidget.h
@@ -7,27 +7,42 @@
 
 struct FOnAttributeChangeData;
 
+DECLARE_EVENT_TwoParams(UActorBindWidget, FOnActorChangedEvent, AActor*, AActor*);
+
 UCLASS()
-class TESTGAME_API UAttributeDisplayWidget : public UUserWidget
+class TESTGAME_API UActorBindWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool BindActor(AActor* InActor);
+	UFUNCTION(BlueprintNativeEvent)
+	void OnBoundActorChanged(AActor* Old, AActor* New);
+	FOnActorChangedEvent& GetActorChangedEvent() { return OnActorChangedEvent; }
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	AActor* BoundActor = nullptr;
+	FOnActorChangedEvent OnActorChangedEvent;
+};
+
+UCLASS()
+class TESTGAME_API UAttributeDisplayWidget : public UActorBindWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnBoundActorChanged_Implementation(AActor* Old, AActor* New) override;
 public:
 	UFUNCTION(BlueprintImplementableEvent)
 	void UpdateCurrentAttributeValue(float InOldValue, float InNewvalue);
 	UFUNCTION(BlueprintImplementableEvent)
 	void UpdateMaxAttributeValue(float InOldValue, float InNewvalue);
+	
 	void OnCurrentAttributeValueChanged(const FOnAttributeChangeData& AttributeChangeData);
 	void OnMaxAttributeValueChanged(const FOnAttributeChangeData& AttributeChangeData);
 
-public:
-	UFUNCTION(BlueprintCallable)
-	void BindAttributeOwner(AActor* InAttributeOwner);
-	UFUNCTION(BlueprintImplementableEvent)
-	void OnAttributeOwnerChanged(AActor* InAttributeOwner);
 protected:
-	UPROPERTY(BlueprintReadOnly)
-	AActor* AttributeOwner = nullptr;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FGameplayAttribute CurrentAttribute;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
