193aa4e472b4287a501f440610eaae8159149077
문경현 <ghoflvhxj@gmail.com>
2025-03-17 04:16:15 +0900

    기타 함수들 추가

--- TestGame/MFunctionLibrary/MMiscFunctionLibrary.cpp
+++ TestGame/MFunctionLibrary/MMiscFunctionLibrary.cpp
@@ -1,6 +1,8 @@
 #include "MMiscFunctionLibrary.h"
 #include "Online/CoreOnline.h"
-
+#include "MGameInstance.h"
+#include "TestGame/MAbility/MEffect.h"
+#include "TestGame/MGameState/MGameStateInGame.h"
 
 bool UMMiscFunctionLibrary::CompareUniqueNetID(APlayerState* Lhs, APlayerState* Rhs)
 {
@@ -14,7 +16,7 @@ bool UMMiscFunctionLibrary::CompareUniqueNetID(APlayerState* Lhs, APlayerState* 
 
 FString UMMiscFunctionLibrary::GetServerIpAddress()
 {
-	FString ConfigFilePath = FPaths::ProjectDir() / TEXT("ServerAddress.txt");
+	FString ConfigFilePath = FPaths::Combine(TEXT("../../../"), TEXT("ServerAddress.txt"));
 	FString ServerAddress = TEXT("127.0.0.1");
 
 	if (FFileHelper::LoadFileToString(ServerAddress, *ConfigFilePath))
@@ -24,4 +26,52 @@ FString UMMiscFunctionLibrary::GetServerIpAddress()
 	}
 
 	return ServerAddress;
+}
+
+UAudioComponent* UMMiscFunctionLibrary::PlayUISoundByEffectIndex(const UObject* WorldContextObject, int32 InEffectIndex)
+{
+	return PlayUISoundByEffectIndexWithConcurrency(WorldContextObject, InEffectIndex, nullptr);
+}
+
+UAudioComponent* UMMiscFunctionLibrary::PlayUISoundByEffectIndexWithConcurrency(const UObject* WorldContextObject, int32 InEffectIndex, USoundConcurrency* Concurrency)
+{
+	const FEffectTableRow& EffectTableRow = UMGameInstance::GetEffectTableRow(WorldContextObject, InEffectIndex);
+
+	TSoftObjectPtr<USoundBase> ObjectPtr(EffectTableRow.Sound);
+	if (USoundBase* Sound = ObjectPtr.LoadSynchronous())
+	{
+		return UGameplayStatics::SpawnSound2D(WorldContextObject, Sound, 1.f, 1.f, 0.f, Concurrency);
+	}
+
+	return nullptr;
+}
+
+void UMMiscFunctionLibrary::PlayBGM(const UObject* WorldContextObject, int32 InEffectIndex)
+{
+	if (WorldContextObject == nullptr)
+	{
+		return;
+	}
+
+	const FEffectTableRow& EffectTableRow = UMGameInstance::GetEffectTableRow(WorldContextObject, InEffectIndex);
+
+	TSoftObjectPtr<USoundBase> ObjectPtr(EffectTableRow.Sound);
+	USoundBase* Sound = ObjectPtr.LoadSynchronous();
+	if (IsValid(Sound) == false)
+	{
+		return;
+	}
+
+	AMGameStateInGame* GameState = WorldContextObject->GetWorld()->GetGameState<AMGameStateInGame>();
+	if (IsValid(GameState) == false)
+	{
+		return;
+	}
+
+	GameState->ChangeBGM(Sound);
+}
+
+float UMMiscFunctionLibrary::UnwindDegree(float InDegree)
+{
+	return FMath::UnwindDegrees(InDegree);
 }
--- TestGame/MFunctionLibrary/MMiscFunctionLibrary.h
+++ TestGame/MFunctionLibrary/MMiscFunctionLibrary.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "Engine/AssetManager.h"
 #include "MMiscFunctionLibrary.generated.h"
 
 UCLASS()
@@ -15,4 +16,47 @@ public:
 
 	UFUNCTION(BlueprintPure)
 	static FString GetServerIpAddress();
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject"))
+	static UAudioComponent* PlayUISoundByEffectIndex(const UObject* WorldContextObject, int32 InEffectIndex);
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject"))
+	static UAudioComponent* PlayUISoundByEffectIndexWithConcurrency(const UObject* WorldContextObject, int32 InEffectIndex, USoundConcurrency* Concurrency);
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject"))
+	static void PlayBGM(const UObject* WorldContextObject, int32 InEffectIndex);
+
+	UFUNCTION(BlueprintPure)
+	static float UnwindDegree(float InDegree);
+
+	static void LoadAssetFrom(const UStruct* InStruct, const void* InSource)
+	{
+		if (InStruct == nullptr)
+		{
+			return;
+		}
+
+		if (InSource == nullptr)
+		{
+			return;
+		}
+
+		FStreamableManager& StreamableManager = UAssetManager::GetStreamableManager();
+		for (TFieldIterator<FProperty> Iter(InStruct); Iter; ++Iter)
+		{
+			if (const FStructProperty* StructProperty = CastField<FStructProperty>(*Iter))
+			{
+				if (StructProperty->Struct == TBaseStructure<FSoftObjectPath>::Get())
+				{
+					if (const FSoftObjectPath* ObjectPath = StructProperty->ContainerPtrToValuePtr<FSoftObjectPath>(InSource))
+					{
+						if (ObjectPath->IsValid())
+						{
+							StreamableManager.RequestAsyncLoad(*ObjectPath, []() {});
+						}
+					}
+				}
+			}
+		}
+	}
 };
